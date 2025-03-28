63774db777b0aeb466c5f4ca39fb2c0acd3d0a37
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-03-27 20:06:56 -0600

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
+import { saveFile, openFile } from './model.mjs';
 
 // Esperamos que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
